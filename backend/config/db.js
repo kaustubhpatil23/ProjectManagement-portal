8e2c45df0c15f13543ff752fc5c43c63664bf729
kaustubhpatil23 <155620241+kaustubhpatil23@users.noreply.github.com>
2026-04-19 16:15:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = mysql.createPool(urlDB);
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 // ADD THIS QUICK TEST:
 pool.getConnection((err, connection) => {
